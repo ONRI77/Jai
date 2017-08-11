1fdb1dccb4f156224f05218a3b3e28295377bb9c
ONRI77 <30368471+ONRI77@users.noreply.github.com>
2017-08-11 16:33:03 +0530

    Create posneg.c

---
+++ posneg.c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+int main()
+{
+int i:
+printf("\n Enter the number:%d");
+scanf("%d",&i);
+if(i>0)
+{
+printf("\n The number is positive");
+}
+else
+{
+printf("\n The number is negative");
+}
+return(0);
+}
